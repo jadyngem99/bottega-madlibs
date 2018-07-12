caf318909ff75d7fcd6e90abacee34a7ad05d804
Jordan Hudgens <jordan@devcamp.com>
2018-07-12 12:07:08 -0600

    creted a css grid for card and inputs

--- src/components/card.js
+++ src/components/card.js
@@ -94,7 +94,9 @@ class Card extends Component {
     return (
       <form onSubmit={this.handleFormSubmit} className="card">
         <div className="card__inputs">
-          {inputData.map(data => Input(data, this.handleInputChange))}
+          {inputData.map((data, index) => {
+            return Input(data, this.handleInputChange, index);
+          })}
         </div>
 
         <button type="submit">
